b84c337c6e82e7a1dc991e577341d9686ed973cd
Farzin Far <farzinfweb@gmail.com>
2025-01-20 23:52:26 +0330

    feat: init main faker

--- fakery.go
+++ fakery.go
@@ -1,6 +1,8 @@
 package fakery
 
 import (
+	"github.com/47monad/fakery/fk/fklorem"
+	"github.com/47monad/fakery/fk/fkopts"
 	"golang.org/x/text/language"
 )
 
@@ -8,3 +10,23 @@ var (
 	fallbackLang = language.English
 	defaultLang  = language.English
 )
+
+type Faker struct {
+	lw *fkopts.LangWrapper
+}
+
+func New() *Faker {
+	return &Faker{}
+}
+
+func (f *Faker) Word(opts ...fkopts.Builder[fkopts.WordOpts]) string {
+	return fklorem.Word(opts...)
+}
+
+func (f *Faker) Words(opts ...fkopts.Builder[fkopts.WordsOpts]) []string {
+	return fklorem.Words(opts...)
+}
+
+func (f *Faker) Sentence(opts ...fkopts.Builder[fkopts.SentenceOpts]) string {
+	return fklorem.Sentence(opts...)
+}
